53ebdf6683436c6dc13961a4f2c073dec000aafb
x2gut <zorganhux@gmail.com>
2024-09-16 22:41:41 +0200

    renaming and little improvements

--- src/helpers/getAccuray.ts
+++ src/helpers/getAccuray.ts
@@ -1,3 +1,15 @@
-export const getAccuracy = (totalChars: number, mistakes: number) => {
-    return ((totalChars - mistakes) / totalChars) * 100;
-}
+export const getAccuracy = (
+    wordsAmount: number,
+    avgWordsLength: number,
+    mistakes: number,
+    correctChars: number
+  ) => {
+    const totalChars = correctChars + mistakes;
+    if (correctChars === 0) {
+      return 0;
+    }
+    const accuracy = (correctChars / totalChars) * 100;
+    
+    return accuracy < 0 ? 0 : accuracy;
+  };
+  
