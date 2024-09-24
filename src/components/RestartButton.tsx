bd5a28502535d75a0ca24db24d02cd52220dd125
x2gut <zorganhux@gmail.com>
2024-09-24 23:17:09 +0200

    added focus on displayWords after restart

--- src/components/RestartButton.tsx
+++ src/components/RestartButton.tsx
@@ -1,17 +1,21 @@
-import React, { useContext, useMemo } from "react";
+import React, { useMemo, useRef } from "react";
 import { MdOutlineRestartAlt } from "react-icons/md";
 import { useTheme } from "../contexts/ThemeProvider";
 
 const RestartButton: React.FC<{ onClick: () => void }> = ({ onClick }) => {
-  const { theme, handleThemeChange } = useTheme();
   const { colors } = useTheme();
+  const restarnBtnRef = useRef<HTMLButtonElement>(null);
 
   const memorizedComponent = useMemo(() => {
     return (
       <button
+        ref={restarnBtnRef}
         className="restart-btn my-3 p-3 relative outline-none"
         onClick={() => {
           onClick();
+          if (restarnBtnRef.current) {
+            restarnBtnRef.current.blur()
+          }
         }}
         tabIndex={2}
       >
