94eab2ebe3143b3a01b99f1bf97e6606f69c6c8a
x2gut <zorganhux@gmail.com>
2024-09-24 23:17:15 +0200

    search input added

--- src/components/ThemesSidebar.tsx
+++ src/components/ThemesSidebar.tsx
@@ -1,8 +1,8 @@
-import { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { useTheme } from "../contexts/ThemeProvider";
 import { themes } from "../themes/themes";
 import { MdKeyboardDoubleArrowRight } from "react-icons/md";
-import { MdKeyboardDoubleArrowDown } from "react-icons/md";
+import { HiMagnifyingGlass } from "react-icons/hi2";
 
 interface ThemeSidebarProps {
   className: string;
@@ -10,10 +10,20 @@ interface ThemeSidebarProps {
 
 const ThemesSidebar: React.FC<ThemeSidebarProps> = ({ className }) => {
   const { handleThemeChange } = useTheme();
-  const currentTheme = localStorage.getItem("theme");
-  const { colors } = useTheme();
+  const { colors, theme } = useTheme();
   const sidebarRef = useRef<HTMLDivElement>(null);
   const arrowBtnRef = useRef<HTMLButtonElement>(null);
+  const [searchTerm, setSearchTerm] = useState<string>("");
+
+  const handleSearchInputChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const filteredData = Object.keys(themes).filter((item) =>
+    item.toLowerCase().includes(searchTerm)
+  );
 
   const handleSidebarToggle = () => {
     if (sidebarRef.current && arrowBtnRef.current) {
@@ -21,6 +31,23 @@ const ThemesSidebar: React.FC<ThemeSidebarProps> = ({ className }) => {
       arrowBtnRef.current.classList.toggle("rotated");
     }
   };
+  const handleClickOutside = (event: MouseEvent) => {
+    if (
+      sidebarRef.current &&
+      !sidebarRef.current.contains(event.target as Node) &&
+      !arrowBtnRef.current?.contains(event.target as Node)
+    ) {
+      sidebarRef.current.classList.add("collapsed");
+      arrowBtnRef.current?.classList.remove("rotated");
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
 
   return (
     <div
@@ -29,14 +56,25 @@ const ThemesSidebar: React.FC<ThemeSidebarProps> = ({ className }) => {
     >
       <div className="themes-sidebar h-72 w-52 rounded-3xl relative">
         <div className="themes-sidebar-content px-5 flex flex-col">
-          {Object.keys(themes).map((value) => {
+          <div className="search-container m-auto relative">
+            <input
+              type="text"
+              placeholder="Search theme"
+              className="rounded-md p-2 outline-none mb-3 relative max-w-40 pl-8"
+              onChange={handleSearchInputChange}
+            />
+            <button>
+              <HiMagnifyingGlass className="absolute left-2 top-3" />
+            </button>
+          </div>
+          {filteredData.map((value) => {
             return (
               <button
                 style={{
                   color: themes[value]["--main-color"],
                   backgroundColor: themes[value]["--bg-color"],
                   border:
-                    value === currentTheme
+                    value === theme
                       ? `1px solid ${themes[value]["--sub-color"]}`
                       : "",
                 }}
