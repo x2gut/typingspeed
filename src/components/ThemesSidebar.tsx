2b8a0a6d9a25e3af2a15c2dfb44daa5c65684f60
x2gut <zorganhux@gmail.com>
2024-10-05 16:04:38 +0200

    search input is now common for sidebar and languages modal

--- src/components/ThemesSidebar.tsx
+++ src/components/ThemesSidebar.tsx
@@ -3,23 +3,20 @@ import { useTheme } from "../contexts/ThemeProvider";
 import { themes } from "../themes/themes";
 import { MdKeyboardDoubleArrowRight } from "react-icons/md";
 import { HiMagnifyingGlass } from "react-icons/hi2";
+import SearchInput from "./SearchInput";
+import ThemeButton from "./ThemeBtn";
+import { useTypeSettings } from "../contexts/TypeSettingsContext";
 
 interface ThemeSidebarProps {
   className: string;
 }
 
 const ThemesSidebar: React.FC<ThemeSidebarProps> = ({ className }) => {
-  const { handleThemeChange } = useTheme();
-  const { colors, theme } = useTheme();
+  const { colors } = useTheme();
   const sidebarRef = useRef<HTMLDivElement>(null);
   const arrowBtnRef = useRef<HTMLButtonElement>(null);
   const [searchTerm, setSearchTerm] = useState<string>("");
-
-  const handleSearchInputChange = (
-    event: React.ChangeEvent<HTMLInputElement>
-  ) => {
-    setSearchTerm(event.target.value);
-  };
+  const { typeSettings } = useTypeSettings();
 
   const filteredData = Object.keys(themes).filter((item) =>
     item.toLowerCase().includes(searchTerm)
@@ -31,6 +28,7 @@ const ThemesSidebar: React.FC<ThemeSidebarProps> = ({ className }) => {
       arrowBtnRef.current.classList.toggle("rotated");
     }
   };
+
   const handleClickOutside = (event: MouseEvent) => {
     if (
       sidebarRef.current &&
@@ -50,57 +48,40 @@ const ThemesSidebar: React.FC<ThemeSidebarProps> = ({ className }) => {
   }, []);
 
   return (
-    <div
-      className={`themes-sidebar-wrapper collapsed absolute left-0 z-10 bottom-1/2 transition-all ${className}`}
-      ref={sidebarRef}
-    >
-      <div className="themes-sidebar h-72 w-52 rounded-3xl relative">
-        <div className="themes-sidebar-content px-5 flex flex-col">
-          <div className="search-container m-auto relative">
-            <input
-              type="text"
-              placeholder="Search theme"
-              className="rounded-md p-2 outline-none mb-3 relative max-w-40 pl-8"
-              onChange={handleSearchInputChange}
-            />
-            <button>
-              <HiMagnifyingGlass className="absolute left-2 top-3" />
-            </button>
+    <>
+      {typeSettings.themesSidebar && (
+        <div
+          className={`themes-sidebar-wrapper collapsed absolute left-0 z-10 bottom-1/2 transition-all ${className}`}
+          ref={sidebarRef}
+        >
+          <div className="themes-sidebar h-72 w-52 rounded-3xl relative">
+            <div className="themes-sidebar-content px-5 flex flex-col">
+              <div className="search-container m-auto relative">
+                <SearchInput
+                  setSearchTerm={setSearchTerm}
+                  className="rounded-md p-2 outline-none mb-3 relative max-w-40 pl-8"
+                />
+                <HiMagnifyingGlass className="absolute left-2 top-3" />
+              </div>
+              {filteredData.map((value) => {
+                return <ThemeButton key={value} value={value} />;
+              })}
+            </div>
           </div>
-          {filteredData.map((value) => {
-            return (
-              <button
-                style={{
-                  color: themes[value]["--main-color"],
-                  backgroundColor: themes[value]["--bg-color"],
-                  border:
-                    value === theme
-                      ? `1px solid ${themes[value]["--sub-color"]}`
-                      : "",
-                }}
-                className="theme p-2 mb-3 rounded-lg hover:scale-105 transition-all 0.2s tracking-widest"
-                key={value}
-                onClick={() => {
-                  handleThemeChange(value);
-                }}
-              >
-                {value}
-              </button>
-            );
-          })}
+          <button
+            className="sidebar-arrow-btn absolute bottom-0 -right-1/4 hover:translate-x-1 duration-75"
+            ref={arrowBtnRef}
+          >
+            <MdKeyboardDoubleArrowRight
+              size={28}
+              color={colors.mainColor}
+              onClick={handleSidebarToggle}
+            />
+          </button>
         </div>
-      </div>
-      <button
-        className="sidebar-arrow-btn absolute bottom-0 -right-1/4 hover:translate-x-1 duration-75"
-        ref={arrowBtnRef}
-      >
-        <MdKeyboardDoubleArrowRight
-          size={28}
-          color={colors.mainColor}
-          onClick={handleSidebarToggle}
-        />
-      </button>
-    </div>
+      )}
+    </>
   );
 };
+
 export default ThemesSidebar;
--- src/modals/languageSelectModal.tsx
+++ src/modals/languageSelectModal.tsx
@@ -1,5 +1,14 @@
-import { Dispatch, SetStateAction, useEffect, useRef } from "react";
+import { Dispatch, SetStateAction, useEffect, useRef, useState } from "react";
 import { useTypeSettings } from "../contexts/TypeSettingsContext";
+import { GoSearch } from "react-icons/go";
+import { useTheme } from "../contexts/ThemeProvider";
+import SearchInput from "../components/SearchInput";
+
+interface LanguageOption {
+  id: string;
+  label: string;
+  layout: string;
+}
 
 interface LanguageSelectModalProps {
   isActive: boolean;
@@ -14,6 +23,23 @@ const LanguageSelectModal: React.FC<LanguageSelectModalProps> = ({
 }) => {
   const modalRef = useRef<HTMLDivElement>(null);
   const { setTypeSettings } = useTypeSettings();
+  const { colors } = useTheme();
+  const [searchTerm, setSearchTerm] = useState<string>("");
+
+  const languages: LanguageOption[] = [
+    { id: "english", label: "English", layout: "english" },
+    { id: "english_1k", label: "English 1k", layout: "english" },
+    { id: "english_10k", label: "English 10k", layout: "english" },
+    { id: "russian", label: "Russian", layout: "russian" },
+    { id: "russian_1k", label: "Russian 1k", layout: "russian" },
+    { id: "russian_10k", label: "Russian 10k", layout: "russian" },
+    { id: "ukrainian", label: "Ukrainian", layout: "ukrainian" },
+  ];
+
+  const filteredLanguages = languages.filter((language) =>
+    language.label.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   useEffect(() => {
     const handleClickOutside = (event: MouseEvent) => {
       if (modalRef.current && !modalRef.current.contains(event.target as Node))
@@ -24,80 +50,41 @@ const LanguageSelectModal: React.FC<LanguageSelectModalProps> = ({
     return () => {
       document.removeEventListener("mousedown", handleClickOutside);
     };
-  }, []);
+  }, [setIsActive]);
 
-  const changeLang = (newLang: string) => {
+  const changeLang = (newLang: string, layout: string) => {
     setIsActive(false);
     setTypeSettings((prevData) => ({
       ...prevData,
       lang: newLang,
-    }));
-  };
-
-  const changeLayout = (layout: string) => {
-    setIsActive(false);
-    setTypeSettings((prevData) => ({
-      ...prevData,
       layout: layout,
     }));
+    resetGame();
   };
 
   return (
     <>
       {isActive && (
         <div className="language-modal w-screen h-screen fixed top-0 left-0 flex justify-center">
-          <div
-            className="language-modal-content rounded-r-lg pt-10"
-            ref={modalRef}
-          >
-            <div
-              className="language-option"
-              onClick={() => {
-                changeLang("english");
-                changeLayout("english");
-                resetGame();
-              }}
-            >
-              <button className="pl-7 py-1" id="english">
-                english
+          <div className="language-modal-content rounded-r-lg" ref={modalRef}>
+            <GoSearch
+              size={20}
+              color={colors.mainColor}
+              className="absolute translate-y-3 translate-x-2"
+            />
+            <SearchInput
+              className="language-search w-full h-12 pl-10 bg-transparent outline-none text-xl"
+              setSearchTerm={setSearchTerm}
+            />
+            {filteredLanguages.map((language) => (
+              <button
+                key={language.id}
+                className="language-option pl-7 py-1 w-full text-start"
+                onClick={() => changeLang(language.id, language.layout)}
+              >
+                {language.label}
               </button>
-            </div>
-            <div
-              className="language-option"
-              onClick={() => {
-                changeLang("english_1k");
-                changeLayout("english");
-                resetGame();
-              }}
-            >
-              <button className="pl-7 py-1" id="english_1k">
-                english 1k
-              </button>
-            </div>
-            <div
-              className="language-option"
-              onClick={() => {
-                changeLang("english_10k");
-                changeLayout("english");
-                resetGame();
-              }}
-            >
-              <button className="pl-7 py-1" id="english_10k">
-                english 10k
-              </button>
-            </div>
-            <div
-              className="language-option"
-              onClick={() => {
-                changeLang("russian");
-                changeLayout("russian");
-                resetGame();
-              }}
-            >
-              <button className="pl-7 py-1" id="rusian">
-                russian
-              </button>
-            </div>
+            ))}
           </div>
         </div>
       )}
