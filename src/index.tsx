cadd9728334b6da609372c74244136bd46d203bb
x2gut <zorganhux@gmail.com>
2024-10-13 17:47:14 +0200

    NoticeProvider & QueryClientProvider & AuthProvider added

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,25 @@
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { TypeSettingsProvider } from "./contexts/TypeSettingsContext";
 import { ThemeProvider } from "./contexts/ThemeProvider";
+import { AuthProvider } from "./contexts/authContext";
+import { NoticeProvider } from "./contexts/NoticeContext";
+import { QueryClient, QueryClientProvider } from "react-query";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
+
+export const queryClient = new QueryClient();
+
 root.render(
-  <TypeSettingsProvider>
-    <ThemeProvider>
-      <App />
-    </ThemeProvider>
-  </TypeSettingsProvider>
+  <NoticeProvider>
+    <AuthProvider>
+      <ThemeProvider>
+        <QueryClientProvider client={queryClient}>
+          <App />
+        </QueryClientProvider>
+      </ThemeProvider>
+    </AuthProvider>
+  </NoticeProvider>
 );
