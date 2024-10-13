87377ca26410bdb580f062adf692724b2c713b4c
x2gut <zorganhux@gmail.com>
2024-10-13 17:58:39 +0200

    new routes -> Profile & NotFound404

--- src/routers/routers.tsx
+++ src/routers/routers.tsx
@@ -1,19 +1,35 @@
 import { createBrowserRouter } from "react-router-dom";
 import TypeTest from "../pages/TypeTest";
 import Settings from "../pages/Settings";
+import Layout from "../layouts/Layout";
+import NotFound404 from "../pages/404";
+import Profile from "../pages/Profile";
 
 export const mainRouter = createBrowserRouter([
   {
     path: "/",
-    element: <TypeTest />,
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <TypeTest />,
+      },
+      {
+        path: "/typingspeed",
+        element: <TypeTest />,
+      },
+      {
+        path: "/settings",
+        element: <Settings />,
+      },
+      {
+        path: "*",
+        element: <NotFound404 />,
+      },
+      {
+        path: "/profile",
+        element: <Profile />
+      }
+    ],
   },
-  {
-    path: "/typingspeed",
-    element: <TypeTest />,
-  },
-  {
-    path: "/settings",
-    element: <Settings />
-  }
 ]);
-
