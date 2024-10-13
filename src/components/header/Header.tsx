e802b1f0da60dd9a6bea9a98ac564fed07f7ff88
x2gut <zorganhux@gmail.com>
2024-10-13 17:54:42 +0200

    Styles changes & Login Modal added & AuthContext added

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -2,34 +2,54 @@ import React, { useState } from "react";
 import { FaKeyboard } from "react-icons/fa";
 import { FaRegUser } from "react-icons/fa";
 import { IoSettings } from "react-icons/io5";
-import RegisterModal from "../../modals/registerModal";
+import RegisterModal from "../../modals/RegisterModal";
 import { Link } from "react-router-dom";
 import { useTheme } from "../../contexts/ThemeProvider";
+import LoginModal from "../../modals/LoginModal";
+import { useAuth } from "../../contexts/authContext";
+import AuthBtn from "./AuthBtn";
+import UserProfileBtn from "./UserProfile";
 
 const Header: React.FC = () => {
-  const {colors} = useTheme();
-  const [isRegister, setIsRegister] = useState(false)
+  const { colors } = useTheme();
+  const [isRegister, setIsRegister] = useState(false);
+  const [isLogin, setIsLogin] = useState(false);
+  const { isAuthenticated, username } = useAuth();
 
   return (
-    <header className={`container header flex justify-between py-4 transition-opacity`}>
+    <header
+      className={`container header flex justify-between py-3 transition-opacity`}
+    >
       <div className="header-left flex gap-6 items-center">
-      <RegisterModal isRegister={isRegister} setIsRegister={setIsRegister}/>
+        <RegisterModal
+          isRegister={isRegister}
+          setIsRegister={setIsRegister}
+          setIsLogin={setIsLogin}
+        />
+        <LoginModal
+          isLogin={isLogin}
+          setIsLogin={setIsLogin}
+          setIsRegister={setIsRegister}
+        />
         <div className="header-logo">
-          <Link className="flex items-center gap-3" to="/">
-            <FaKeyboard size={52} color={colors.mainColor}/>
-            <p className="text-3xl font-bold">TypeTest</p>
+          <Link className="flex items-center justify-center gap-3" to="/">
+            <FaKeyboard size={36} color={colors.mainColor} />
+            <p className="text-[30px] font-bold">TypeTest</p>
           </Link>
         </div>
         <Link to="/settings" className="header-settings">
-          <IoSettings size={20} className="hover:rotate-90 transition-all duration-300" />
+          <IoSettings
+            size={20}
+            className="hover:rotate-90 transition-all duration-300"
+          />
         </Link>
       </div>
-      <div className="header-right flex items-center">
-        <div className="user">
-          <button className="header-user" onClick={() => setIsRegister(!isRegister)}>
-            <FaRegUser size={28} color={colors.mainColor}/>
-          </button>
-        </div>
+      <div className="header-right flex items-center justify-center">
+        {isAuthenticated ? (
+          <UserProfileBtn />
+        ) : (
+          <AuthBtn isRegister={isRegister} setIsRegister={setIsRegister} />
+        )}
       </div>
     </header>
   );
