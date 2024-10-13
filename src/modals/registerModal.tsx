257672ee51c0a2bbc6a910cc34a590a0756a330c
x2gut <zorganhux@gmail.com>
2024-10-13 17:57:17 +0200

    Api requests added using react-query

--- src/modals/registerModal.tsx
+++ src/modals/registerModal.tsx
@@ -1,29 +1,95 @@
 import React, { Dispatch, useEffect, useRef, useState } from "react";
 import { AiOutlineUserAdd } from "react-icons/ai";
+import registerUser from "../api/register";
+import loginUser from "../api/login";
+import { useNotice } from "../contexts/NoticeContext";
+import { useAuth } from "../contexts/authContext";
+import { useMutation } from "react-query";
 
 interface RegisterModalInterface {
   isRegister: boolean;
   setIsRegister: Dispatch<React.SetStateAction<boolean>>;
+  setIsLogin: Dispatch<React.SetStateAction<boolean>>;
 }
 
 const RegisterModal: React.FC<RegisterModalInterface> = ({
   isRegister,
   setIsRegister,
+  setIsLogin,
 }) => {
   const [username, setUsername] = useState<string>("");
-  const [isUsernameError, setIsUsernameError] = useState<string>("");
   const [password, setPassword] = useState<string>("");
-  const [isPasswordError, setIsPasswordError] = useState<string>("");
   const [repeatPassword, setRepeatPassword] = useState<string>("");
   const [email, setEmail] = useState<string>("");
-  const [isEmailError, setIsEmailError] = useState<string>("");
+  const { showNotice } = useNotice();
+  const { setIsAuthenticated } = useAuth();
+
+  const mutation = useMutation(registerUser, {
+    onSuccess: () => {
+      showNotice("You successfully registered", "success", 5000);
+      clearFields();
+      loginUser({ username, password });
+      setIsAuthenticated(true);
+      setIsRegister(false);
+    },
+
+    onError: (error: { error: string }[]) => {
+      if (error.length > 0) {
+        error.forEach((errorItem) =>
+          showNotice(errorItem.error, "error", 5000)
+        );
+      }
+    },
+  });
 
   const modalRef = useRef<HTMLFormElement>(null);
 
+  const clearFields = () => {
+    setUsername("");
+    setPassword("");
+    setRepeatPassword("");
+    setEmail("");
+  };
+
+  const checkFields = () => {
+    if (
+      username.length === 0 ||
+      password.length === 0 ||
+      repeatPassword.length === 0 ||
+      email.length === 0
+    ) {
+      return false;
+    }
+    return true;
+  };
+
+  const handleRegister = () => {
+    if (!checkFields()) {
+      showNotice("Fields can't be empty!", "error", 5000);
+      return;
+    }
+
+    if (password !== repeatPassword) {
+      showNotice("Passwords do not match!", "error", 5000);
+      return;
+    }
+
+    mutation.mutate({
+      username,
+      email,
+      password,
+    });
+  };
+
   useEffect(() => {
     const handleClickOutside = (event: MouseEvent) => {
-      if (modalRef.current && !modalRef.current.contains(event.target as Node))
+      if (
+        modalRef.current &&
+        !modalRef.current.contains(event.target as Node)
+      ) {
         setIsRegister(false);
+        clearFields();
+      }
     };
 
     document.addEventListener("mousedown", handleClickOutside);
@@ -32,68 +98,75 @@ const RegisterModal: React.FC<RegisterModalInterface> = ({
     };
   }, []);
 
-  const usernameChangeHandler = (
-    event: React.ChangeEvent<HTMLInputElement>
-  ) => {
-    const usernameMinLength = 4;
-    const usernameMaxLength = 16;
-
-    setUsername(event.target.value);
-
-    if (username.length < usernameMinLength) {
-      setIsUsernameError("Min username length is 4");
-      return;
-    } else if (username.length > usernameMaxLength) {
-      setIsUsernameError("Max username length is 16");
-      return;
-    }
-    setIsUsernameError("");
-  };
-
   return (
     <>
       {isRegister && (
-        <div className="register-modal w-full h-full fixed left-0 top-0 flex items-center justify-center">
+        <div className="modal w-full h-full fixed left-0 top-0 flex items-center justify-center">
           <form
-            action=""
-            className="register-modal-form flex justify-center items-center gap-3 flex-col max-w-80 max-h-80"
+            onSubmit={(event) => {
+              event.preventDefault();
+              handleRegister();
+            }}
+            className="modal-form flex justify-center items-center gap-3 flex-col max-w-80 max-h-80"
             ref={modalRef}
           >
-            <div className="reg-modal-title flex gap-1 items-center">
+            <div className="modal-title flex gap-1 items-center">
               <AiOutlineUserAdd size={32} />
               <h3 className="register-title w-full text-start">register</h3>
             </div>
             <input
               type="text"
-              className="register-field register-username w-72"
+              className="modal-field register-username w-72"
               placeholder="username"
               onChange={(event) => {
-                usernameChangeHandler(event);
+                setUsername(event.target.value);
               }}
               value={username}
             />
             <input
               type="email"
-              className="register-field register-email w-72"
+              className="modal-field register-email w-72"
               placeholder="email"
               onChange={(event) => setEmail(event.target.value)}
               value={email}
             />
             <input
               type="password"
-              className="register-field register-pass w-72"
+              className="modal-field register-pass w-72"
               placeholder="password"
               onChange={(event) => setPassword(event.target.value)}
               value={password}
             />
             <input
               type="password"
-              className="register-field register-repeat-pass w-72"
+              className="modal-field register-repeat-pass w-72"
               placeholder="repeat password"
               onChange={(event) => setRepeatPassword(event.target.value)}
               value={repeatPassword}
             />
-            <button className="register-submit-btn w-72">Register</button>
+            <p>
+              Already have an account?{" "}
+              <button
+                className="underline hover:brightness-125"
+                onClick={() => {
+                  setIsRegister(false);
+                  setIsLogin(true);
+                }}
+              >
+                Login
+              </button>
+            </p>
+            <button
+              className="overflow-hidden flex items-center justify-center relative w-72 bg-[--sub-accent-color]
+             text-[--sub-color] py-4 px-4 rounded-xl font-bold uppercase -- before:block before:absolute before:h-full
+              before:w-1/2 before:rounded-full before:bg-[--main-color] before:top-0 before:left-1/4
+               before:transition-transform before:opacity-0 before:hover:opacity-100 hover:text-[--correct-text-color]
+                hover:before:animate-ping transition-all duration-300"
+            >
+              <button disabled={mutation.isLoading} className="relative">
+                {mutation.isLoading ? "Registering..." : "Register"}
+              </button>
+            </button>
           </form>
         </div>
       )}
