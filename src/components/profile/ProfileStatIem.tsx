e8939882c4021fa0f9a00eccfeb9bbde6f4b3fcf
x2gut <zorganhux@gmail.com>
2024-10-29 16:23:14 +0100

    new feature -> profile statistics

---
+++ src/components/profile/ProfileStatIem.tsx
@@ -0,0 +1,38 @@
+import { FaCrown } from "react-icons/fa6";
+import { CiWavePulse1 } from "react-icons/ci";
+
+interface ProfileStatItemProps {
+  time: number;
+  words: number;
+  best: number;
+  avg: number;
+}
+
+const ProfileStatItem: React.FC<ProfileStatItemProps> = ({
+  time,
+  words,
+  best,
+  avg,
+}) => {
+  return (
+    <div className="bg-[--bg-color] brightness-[90%] w-[180px] h-[150px] p-3 rounded-md">
+      <h6 className="text-lg text-[--sub-color] text-center mb-2">{words !== 0 ? words : time} {words !== 0 ? "Words" : "Seconds"}</h6>
+      <div className="text-lg flex flex-col gap-3 text-[--sub-color]">
+        <div className="bg-[--bg-color] brightness-125 p-1 rounded-md flex items-center gap-2">
+          {" "}
+          <FaCrown className="fill-[--main-color]"/>
+          <span className="text-[--text-correct-color]">BEST </span>{" "}
+          <span className="text-[--sub-accent-color]">{best} WPM</span>
+        </div>
+        <div className="bg-[--bg-color] brightness-125 p-1 rounded-md flex items-center gap-2">
+          {" "}
+          < CiWavePulse1 />
+          <span className="text-[--text-correct-color]">AVG </span>
+          <span className="text-[--sub-accent-color]">{avg} WPM</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProfileStatItem;
--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,37 +1,95 @@
-import { useEffect, useState } from "react";
-import { getUserProfilePicture } from "../api/userProfile";
+import { useEffect, useRef, useState } from "react";
 import { useAuth } from "../contexts/authContext";
 import { useNavigate } from "react-router-dom";
+import { IoAddCircle } from "react-icons/io5";
+import { useTheme } from "../contexts/ThemeProvider";
+import { MdEmail } from "react-icons/md";
+import { GrStatusCriticalSmall } from "react-icons/gr";
+import useProfile from "../hooks/useProfile";
+import { useNotice } from "../contexts/NoticeContext";
+import ProfileStatistics from "../components/profile/ProfileSatistics";
+import ProfileHistory from "../components/profile/History";
 
 const Profile = () => {
-  const { isAuthenticated } = useAuth();
-  const [userPicture, setUserPicture] = useState<string>("");
+  const [limit, setLimit] = useState(10);
+  const { isAuthenticated, userId } = useAuth();
+  const { showNotice } = useNotice();
+  const { colors } = useTheme();
   const navigate = useNavigate();
+  const pictureChangeInputRef = useRef<HTMLInputElement | null>(null);
+  const { userPicture, profileData, avgStats, history, handlePictureChange } =
+    useProfile(showNotice, userId, limit);
 
   useEffect(() => {
     if (!isAuthenticated) {
       navigate("/");
     }
-
-    const profilePic = async () => {
-      const url = await getUserProfilePicture();
-      if (url) {
-        console.log(url)
-        setUserPicture(url)
-      }
-    };
-
-    profilePic();
-  }, []);
+  }, [isAuthenticated]);
 
   return (
-    <div className="container flex flex-row gap-28">
-      <div className="profile-left bg-[--bg-color] brightness-90 w-96 h-96">
-        <div className="avatar max-w-16 max-h-16">
-          <img src={userPicture} alt="userProfile" />
+    <div className="container">
+      <div className="profile-upper flex flex-row gap-28">
+        <div className="profile-left flex flex-col items-center pt-5 bg-[--bg-color] brightness-90 w-[50%] h-96">
+          <div className="user-id text-3xl pl-5 opacity-50 font-light text-[--text-correct-color] w-full">
+            {profileData.id}
+          </div>
+          <div
+            className={
+              "avatar relative w-20 h-20 rounded-full pb-5 bg-center bg-cover"
+            }
+            style={{
+              backgroundImage: `url(${userPicture})`,
+            }}
+          >
+            <button>
+              <IoAddCircle
+                size={24}
+                className="absolute bottom-0 right-0"
+                color={colors.subColor}
+                onClick={() => {
+                  if (pictureChangeInputRef.current) {
+                    pictureChangeInputRef.current.click();
+                  }
+                }}
+              />
+            </button>
+            <input
+              type="file"
+              className="hidden"
+              ref={pictureChangeInputRef}
+              accept=".jpg,.jpeg,.png"
+              onChange={handlePictureChange}
+            />
+          </div>
+          <div className="username text-2xl font-light text-[--text-correct-color] pb-5">
+            {profileData.username}
+          </div>
+          <div className="information w-full p-2">
+            <div className="flex items-center gap-2 text-2xl font-light text-[--text-correct-color] pb-5 break-words w-full max-w-full">
+              <div className="flex gap-1 items-center">
+                <MdEmail />
+                email:
+              </div>
+              {profileData.email}
+            </div>
+            <div
+              className={`flex items-center gap-2 active text-2xl font-light pb-5 ${
+                profileData.isActive
+                  ? "text-[--text-correct-color]"
+                  : "text-[--mistake-color]"
+              }`}
+            >
+              <span className="flex items-center gap-1 text-[--text-color]">
+                <GrStatusCriticalSmall />
+                user status:
+              </span>
+              {profileData.isActive ? "Confirmed" : "Not confirmed"}
+            </div>
+          </div>
         </div>
+        <ProfileStatistics avgStats={avgStats} />
       </div>
-      <div className="profile-right bg-[--bg-color] brightness-90 w-96 h-96"></div>
+      <ProfileHistory history={history} limit={limit} setLimit={setLimit}/>
     </div>
   );
 };
