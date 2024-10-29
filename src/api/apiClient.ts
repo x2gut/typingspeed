06b2bcbbf6dbec28cd0842053b496601fa24a06b
x2gut <zorganhux@gmail.com>
2024-10-29 16:12:53 +0100

    interceptors moved to apiClient

--- src/api/apiClient.ts
+++ src/api/apiClient.ts
@@ -1,11 +1,59 @@
-import axios from 'axios';
+import axios from "axios";
 
 const apiClient = axios.create({
   baseURL: "http://localhost:8000",
-  timeout: 5000,
-  headers: {
-    'Content-Type': 'application/json',
-  },
+  timeout: 1000,
 });
+
+apiClient.interceptors.request.use(
+  (request) => {
+    const accessToken = localStorage.getItem("access_token");
+    if (accessToken) {
+      request.headers["Authorization"] = `Bearer ${accessToken}`;
+    }
+    return request;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+apiClient.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const originalRequest = error.config;
+    if (error.response.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+      try {
+        const refreshToken = localStorage.getItem("refresh_token");
+        const response = await axios.post(
+          "http://localhost:8000/token/refresh",
+          {},
+          {
+            headers: {
+              Authorization: `Bearer ${refreshToken}`,
+            },
+          }
+        );
+        
+        const { access_token } = response.data;
+        localStorage.setItem("access_token", access_token);
+
+        apiClient.defaults.headers.common[
+          "Authorization"
+        ] = `Bearer ${access_token}`;
+
+        return apiClient(originalRequest);
+
+      } catch (refreshError) {
+        console.error("Token refresh failed:", refreshError);
+        localStorage.removeItem("accessToken");
+        localStorage.removeItem("refreshToken");
+        return Promise.reject(refreshError);
+      }
+    }
+    return Promise.reject(error);
+  }
+);
 
 export default apiClient;
