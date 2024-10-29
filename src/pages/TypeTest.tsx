e87217ae8d868a44c82feaa728c48bab390b2a4c
x2gut <zorganhux@gmail.com>
2024-10-29 16:28:32 +0100

    config query added

--- src/pages/TypeTest.tsx
+++ src/pages/TypeTest.tsx
@@ -1,22 +1,59 @@
 import React, { useEffect, useState } from "react";
 import DisplayWords from "../components/diplayWords/DisplayWords";
-import Header from "../components/header/Header";
 import TypeSettingsMenu from "../components/diplayWords/TypeSettingsMenu";
-import { TypeSettingsProvider } from "../contexts/TypeSettingsContext";
-import { ThemeProvider } from "../contexts/ThemeProvider";
+import {
+  useTypeSettings,
+} from "../contexts/TypeSettingsContext";
 import FooterCommands from "../components/diplayWords/FooterCommands";
 import ThemesSidebar from "../components/diplayWords/ThemesSidebar";
+import { useAuth } from "../contexts/authContext";
+import { useQuery } from "react-query";
+import { getConfig } from "../api/configApi";
+import { useTheme } from "../contexts/ThemeProvider";
+
 const TypeTest: React.FC = () => {
   const [isFocused, setIsFocused] = useState(false);
   const [currentLang, setCurrentLang] = useState("");
   const [words, setWords] = useState<string[]>([]);
+  const { isAuthenticated } = useAuth();
+  const { handleThemeChange } = useTheme();
+  const { setTypeSettings } = useTypeSettings();
+
+  const config = useQuery(["config"], getConfig, {
+    enabled: isAuthenticated,
+    refetchOnWindowFocus: false,
+    onSuccess: (response) => {
+      const config = response.data[0].config[0];
+      const theme = response.data[1];
+
+      handleThemeChange(theme);
+
+      setTypeSettings({
+        layout: config?.layout,
+        lang: config?.lang,
+        mode: config?.mode,
+        time: Number(config.time),
+        words: Number(config.words),
+        keyboard: {
+          show: config?.keyboard.show,
+          responsive: config?.keyboard.responsive,
+        },
+        soundOnPress: config?.soundOnPress,
+        caretType: config?.caretType,
+        caretRainbow: config?.caretRainbow,
+        randomTheme: config?.randomTheme,
+        themesSidebar: config?.themesSidebar,
+        wordsHistory: config?.wordsHistory,
+      });
+    },
+  });
 
   useEffect(() => {
     const result = localStorage.getItem("config")
       ? JSON.parse(localStorage.getItem("config") as string)
       : null;
 
-    setCurrentLang(result.lang);
+    setCurrentLang(result?.lang || "english");
 
     document.title = "TypeTest";
   }, []);
@@ -36,7 +73,7 @@ const TypeTest: React.FC = () => {
   }, [currentLang]);
 
   return (
-    <TypeSettingsProvider>
+    <>
       {words.length > 0 && (
         <div className="container">
           <TypeSettingsMenu className={isFocused ? "focus" : ""} />
@@ -51,7 +88,7 @@ const TypeTest: React.FC = () => {
           <FooterCommands className={isFocused ? "focus" : ""} />
         </div>
       )}
-    </TypeSettingsProvider>
+    </>
   );
 };
 
