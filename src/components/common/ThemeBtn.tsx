1fd8a9eddc7f2e5735602998b7fd65e30c5f48f0
x2gut <zorganhux@gmail.com>
2024-10-29 16:20:22 +0100

    API for theme changing added

--- src/components/common/ThemeBtn.tsx
+++ src/components/common/ThemeBtn.tsx
@@ -1,26 +1,60 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { useTheme } from "../../contexts/ThemeProvider";
 import { themes } from "../../themes/themes";
+import { useMutation } from "react-query";
+import { updateTheme } from "../../api/configApi";
+import { getUserId } from "../../utils/decodeJwt";
+import { useAuth } from "../../contexts/authContext";
+import { useNotice } from "../../contexts/NoticeContext";
 
 interface ThemeButtonProps {
-  value: string;
+  newTheme: string;
   className?: string | undefined;
 }
 
-const ThemeButton: React.FC<ThemeButtonProps> = ({ value, className = "" }) => {
+const ThemeButton: React.FC<ThemeButtonProps> = ({
+  newTheme,
+  className = "",
+}) => {
   const { theme, handleThemeChange } = useTheme();
+  const { isAuthenticated } = useAuth();
+  const token = localStorage.getItem("access_token");
+  const { showNotice } = useNotice();
+  const mutation = useMutation(updateTheme, {
+    onSuccess: () => {
+      console.log("success");
+    },
+    onError: (error) => {
+      showNotice(`Error updating theme: ${error}`, "error", 5000);
+    },
+  });
+
+  const handleClick = () => {
+    handleThemeChange(newTheme);
+    if (token === null || !isAuthenticated) return;
+
+    const user_id = getUserId(token);
+    if (user_id === undefined) return;
+
+    if (newTheme !== theme) {
+      mutation.mutate({ user_id, newTheme });
+    }
+  };
+
   return (
     <button
       style={{
-        color: themes[value]["--main-color"],
-        backgroundColor: themes[value]["--bg-color"],
+        color: themes[newTheme]["--main-color"],
+        backgroundColor: themes[newTheme]["--bg-color"],
         border:
-          value === theme ? `2px solid ${themes[value]["--sub-color"]}` : "",
+          newTheme === theme
+            ? `2px solid ${themes[newTheme]["--sub-color"]}`
+            : "",
       }}
       className={`theme p-2 mb-3 rounded-lg hover:scale-105 transition-all duration-200 tracking-widest ${className}`}
-      onClick={() => handleThemeChange(value)}
+      onClick={handleClick}
     >
-      {value}
+      {newTheme}
     </button>
   );
 };
