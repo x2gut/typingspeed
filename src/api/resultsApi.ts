4c0d57b186767686181f005c7bff7eea8677b363
x2gut <zorganhux@gmail.com>
2024-10-29 16:14:20 +0100

    Api for save and get results added

---
+++ src/api/resultsApi.ts
@@ -0,0 +1,41 @@
+import apiClient from "./apiClient";
+
+export const addResults = async ({
+  user_id,
+  wpm,
+  accuracy,
+  mistakes,
+  time,
+  words,
+  mode,
+  language,
+}: {
+  user_id: number;
+  wpm: number;
+  accuracy: number;
+  mistakes: number;
+  time: number,
+  words: number,
+  mode: string,
+  language: string
+}) => {
+  return apiClient.post("/user/results", {
+    user_id: user_id,
+    wpm: wpm,
+    accuracy: accuracy,
+    mistakes: mistakes,
+    time: time,
+    words: words,
+    mode: mode,
+    language: language,
+  });
+};
+
+
+export const getStats = async (userId: number) => {
+  return apiClient.get(`/user/${userId}/stats`)
+}
+
+export const getHistory = async(userId: number, limit: number = 10, page: number = 1) => {
+  return apiClient.get(`/user/${userId}/history?page=${page}&limit=${limit}`)
+}
