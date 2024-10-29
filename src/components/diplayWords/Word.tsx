7d599fa9cd57389e72f5b186a6ec772afbf2e0cd
x2gut <zorganhux@gmail.com>
2024-10-29 16:21:23 +0100

    incomplete or misspelled words are marked now

--- src/components/diplayWords/Word.tsx
+++ src/components/diplayWords/Word.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { LetterStates } from "../../types/types";
 import { useTypeSettings } from "../../contexts/TypeSettingsContext";
 
@@ -8,48 +8,84 @@ interface WordProps {
   currentLetterIndex: number;
   wordIndex: number;
   letterStates: LetterStates;
+  isGameStarted: boolean;
 }
 
-const Word: React.FC<WordProps> = ({
-  word,
-  currentWordIndex,
-  currentLetterIndex,
-  wordIndex,
-  letterStates,
-}) => {
-  const { typeSettings } = useTypeSettings();
+const Word: React.FC<WordProps> = React.memo(
+  ({
+    word,
+    currentWordIndex,
+    currentLetterIndex,
+    wordIndex,
+    letterStates,
+    isGameStarted,
+  }) => {
+    const { typeSettings } = useTypeSettings();
+    const [wordsState, setWordsState] = useState<{
+      [wordIndex: number]: string;
+    }>({});
 
-  return (
-    <div
-      className={`word m-2 ${wordIndex === currentWordIndex ? "relative" : ""}`}
-      key={wordIndex}
-    >
-      {word.split("").map((letter, letterIndex) => (
-        <span
-          className={`text-3xl ${
-            letterStates[wordIndex]?.[letterIndex] === "correct"
-              ? "correct"
-              : letterStates[wordIndex]?.[letterIndex] === "incorrect"
-              ? "incorrect"
-              : "base"
-          }`}
-          key={letterIndex}
-        >
-          {letter}
-        </span>
-      ))}
-      {typeSettings.caretType && currentWordIndex === wordIndex && (
-        <div
-          className={`caret-${typeSettings.caretType} ${
-            typeSettings.caretRainbow && "caret-rainbow"
-          }`}
-          style={{
-            transform: `translate(${currentLetterIndex * 16.5}px)`,
-          }}
-        ></div>
-      )}
-    </div>
-  );
-};
+    useEffect(() => {
+      const resetWordsState = () => setWordsState({});
+    
+      const getWordState = () => {
+        if (wordIndex >= currentWordIndex) return "";
+    
+        const hasMistake =
+          letterStates[wordIndex] &&
+          Object.values(letterStates[wordIndex]).includes("incorrect");
+    
+        const isIncomplete =
+          letterStates[wordIndex] &&
+          Object.keys(letterStates[wordIndex]).length !== word.length;
+    
+        return hasMistake || isIncomplete ? "word-error" : "";
+      };
+    
+      if (!isGameStarted) {
+        resetWordsState();
+      } else {
+        setWordsState((prevState) => ({
+          ...prevState,
+          [wordIndex]: getWordState(),
+        }));
+      }
+    }, [isGameStarted, letterStates, currentWordIndex]);
+
+    return (
+      <div
+        className={`word m-2 font-normal ${wordsState[wordIndex]} ${
+          wordIndex === currentWordIndex ? "relative" : ""
+        }`}
+        key={wordIndex}
+      >
+        {word.split("").map((letter, letterIndex) => (
+          <span
+            className={`text-3xl ${
+              letterStates[wordIndex]?.[letterIndex] === "correct"
+                ? "correct"
+                : letterStates[wordIndex]?.[letterIndex] === "incorrect"
+                ? "incorrect"
+                : "base"
+            }`}
+            key={letterIndex}
+          >
+            {letter}
+          </span>
+        ))}
+        {typeSettings.caretType && currentWordIndex === wordIndex && (
+          <div
+            className={`caret-${typeSettings.caretType} ${
+              typeSettings.caretRainbow && "caret-rainbow"
+            }`}
+            style={{
+              transform: `translate(${currentLetterIndex * 16.5}px)`,
+            }}
+          ></div>
+        )}
+      </div>
+    );
+  }
+);
 
 export default Word;
